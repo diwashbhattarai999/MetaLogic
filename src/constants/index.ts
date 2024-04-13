0b6ff698db234299a549523369c8f1a6972448c2
Diwash Bhattarai <diwashb999@gmail.com>
2024-04-13 14:06:42 +0545

    init: initialize metalogic web with nextjs

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+const NAV_LINKS = [
+  { title: "Home", link: "/" },
+  { title: "Services", link: "/services" },
+  { title: "Career", link: "/careers" },
+  { title: "Blogs", link: "/blogs" },
+  { title: "About Us", link: "/about" },
+];
