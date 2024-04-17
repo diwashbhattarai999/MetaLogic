cd9ae985901046046f9f623e1e03dcd51e3aca38
Diwash Bhattarai <diwashb999@gmail.com>
2024-04-17 10:00:11 +0545

    feat: add i icon

--- src/components/sections/home/home.tsx
+++ src/components/sections/home/home.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
@@ -13,8 +13,11 @@ import WhyMetalogic from "@/components/sections/home/why-metalogic";
 import ExploreOurProducts from "@/components/sections/home/explore-our-products";
 import OurServices from "@/components/sections/home/our-services";
 import Button from "@/components/ui/button";
+import { Info } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 const Home = () => {
+  const [showInfo, setShowInfo] = useState(false);
   const containerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
@@ -30,15 +33,12 @@ const Home = () => {
         <Hero />
         <SDLC />
       </Container>
-
       <WhyMetalogic />
-
       <Container>
         <ExploreOurProducts />
       </Container>
-
       <OurServices />
-
+      {/* Contact us section */}
       <div className="min-h-[60vh] py-10 flex items-center justify-center flex-col gap-8 relative">
         <div
           className="text-center flex flex-col items-center gap-4 text-secondary-foreground/90 -z-10 max-w-3xl"
@@ -60,6 +60,28 @@ const Home = () => {
               "radial-gradient(800px, rgba(23, 20, 224, 0.4), transparent 80%)",
           }}
         />
+      </div>
+      {/* i for brief explanation of why I chose to recreate Home Page */}
+      <div
+        onMouseEnter={() => setShowInfo(true)}
+        onMouseLeave={() => setShowInfo(false)}
+        className="fixed top-28 right-10 p-1 bg-white rounded-full border border-border/30 shadow-lg cursor-pointer"
+      >
+        <Info className="text-blue-500 w-7 h-7" />
+      </div>
+      1
+      <div
+        className={cn(
+          "duration-300",
+          showInfo
+            ? "opacity-100 pointer-events-auto"
+            : "opacity-0 pointer-events-none"
+        )}
+      >
+        <div className="bg-white border border-border/30 w-5 h-5 rotate-45 fixed top-[10.5rem] right-12 rounded-sm duration-300" />
+        <span className="fixed top-[10.7rem] right-10 flex items-center gap-2 bg-white border border-border/50 rounded-lg p-4 shadow-lg max-w-xl md:text-lg  font-pt">
+          {`I recreated the Home Page to make it visually appealing and user-friendly. I used GSAP for animations, Locomotive Scroll for smooth scrolling, and other techniques to improve the user experience. This redesigned page incorporates GSAP animations and Locomotive Scroll, providing an immersive and captivating experience. With these tools, I enhanced the interface, creating smooth animations and transitions that engage visitors. These improvements not only enhance the aesthetics but also make browsing more intuitive and enjoyable, showing our commitment to innovation and excellence.`}
+        </span>
       </div>
     </>
   );
